ed0675bdca69089e2f651208760386988e7a4773
evan <unyumenye@gmail.com>
2012-10-02 15:16:24 +0800

    'auto'

--- bakup/graphs.php
+++ bakup/graphs.php
@@ -483,6 +483,7 @@ function form_actions() {
 			<script language="JavaScript">
 			//document.forms[0].parent_item_id.options[0].text='---test4';
 				frm = document.forms[0];
+				alert(frm.action);
 				for (var i=0;i<frm.tree_item_id.options.length;i++) {
 					if (frm.tree_item_id.options[i].text == '--- test5'){
 						gotit = 1;
