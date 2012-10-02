c97cb5e205a14e7cc8f47369678c508eedba087c
evan <unyumenye@gmail.com>
2012-10-02 13:03:10 +0800

    'auto'

--- bakup/tree.php
+++ bakup/tree.php
@@ -6,7 +6,7 @@ if ($_COOKIE["thetreenamechild"] == 'drive E')
 	$_COOKIE["thetreenamechild"] = 'drive F';
 else if ($_COOKIE["thetreenamechild"] == 'drive D')
 	$_COOKIE["thetreenamechild"] = 'drive E';
-else if ($_COOKIE["thetreenamechild"] == 'Drive C')
+else if ($_COOKIE["thetreenamechild"] == 'drive C')
 	$_COOKIE["thetreenamechild"] = 'drive D';
 else
 	$_COOKIE["thetreenamechild"] = 'drive C';
