847b9ba022bafef185687dbd5bb763e02dd4faca
evan <unyumenye@gmail.com>
2012-10-08 11:25:25 +0800

    'auto'

--- bakup/graphs.php
+++ bakup/graphs.php
@@ -522,7 +522,7 @@ function form_actions() {
 				if(<?php echo $_SESSION['thedrivedone'] ?>=='16'){
 					window.location = 'host.php'
 				}
-				setTimeout("frm.submit()",200)
+				frm.submit()
 			</script>
 			<?php
 			
