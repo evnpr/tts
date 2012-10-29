d1f2c78273d04679c652e82e3c4377cba8e26dc6
evan <unyumenye@gmail.com>
2012-10-29 12:55:05 +0800

    'auto'

--- create/tree.php
+++ create/tree.php
@@ -398,7 +398,7 @@ function item_remove() {
 	if (isset($_SESSION['dhtml_tree'])) {
 		unset($_SESSION['dhtml_tree']);
 	}
-
+		include("theConfig.php");
 		header("Location: tree.php?action=item_edit&tree_id=".$_SESSION['tree_id']."&parent_id=0");
 //	header("Location: tree.php?action=edit&id=" . $_GET["tree_id"]); exit;
 }
