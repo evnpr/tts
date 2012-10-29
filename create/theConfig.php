0c8eb8e1ca99c4ac37511a94c19fa05c91f8d253
evan <unyumenye@gmail.com>
2012-10-29 13:10:58 +0800

    'auto'

--- create/theConfig.php
+++ create/theConfig.php
@@ -4,10 +4,7 @@
 		$_SESSION['aggregate'] = 0;
 	}
 	
-	$theDrive1 = 'C';
-	$theDrive2 = 'D';
-	$theDrive3 = 'G';
-	$theDrive4 = 'J';
+
 	
 	/*
 	<option value="tr_19">Place on a Tree (--- Account Servers ---)</option>
@@ -17,5 +14,26 @@
 	<option value="tr_18">Place on a Tree (--- Player Servers ---)</option>
 	
 	*/
-	$_SESSION['tree_id'] = '3';
+	
+	
+	
+	#################### INPUT HERE ###############
+																					#
+																					#
+	$_SESSION['tree_id'] = '3';											#
+																					#
+	$theDrive1 = 'C';															#
+	$theDrive2 = 'D';															#
+	$theDrive3 = 'G';														#
+	$theDrive4 = 'J';															#
+																					#
+																					#	
+	###############################################
+	
+	
+	
+	
+	
+	
+	
 	$_SESSION['typeOfIP'] = 'tr_'.$_SESSION['tree_id'];
